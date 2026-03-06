1d2198cfc86dabc1e186871b1d22d716dd7f9dd0
Tanooja <tanoojatanooja8384@gmail.com>
2026-03-06 17:09:30 +0530

    another new commit

--- HelloWorld.sh
+++ HelloWorld.sh
@@ -4,3 +4,8 @@ userone=tanooja
 usertwo=manikanta
 echo "Hi Tanu"
 echo "$usertwo married $userone on 27th nov 2025"
+
+timestamp=$(date)
+echo "starting time is $timestamp"
+slepp 10
+echo "ending time is $timestamp"
