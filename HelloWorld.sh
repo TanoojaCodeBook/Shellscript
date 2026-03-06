6bd7c9f6fd80f13287a917b70ebf990562dd78f3
Tanooja <tanoojatanooja8384@gmail.com>
2026-03-06 17:13:42 +0530

    4th commit

--- HelloWorld.sh
+++ HelloWorld.sh
@@ -5,7 +5,9 @@ usertwo=manikanta
 echo "Hi Tanu"
 echo "$usertwo married $userone on 27th nov 2025"
 
-timestamp=$(date)
-echo "starting time is $timestamp"
-slepp 10
-echo "ending time is $timestamp"
+starttimestamp=$(date)
+echo "starting time is $starttimestamp"
+sleep 10
+endtimestamp=$(date)
+echo "ending time is $endtimestamp"
+echo "total execution time is ($starttimestamp - $endtimestamp)"
